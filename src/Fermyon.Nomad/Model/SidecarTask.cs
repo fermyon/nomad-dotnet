dc12ec78a52b60d94ef609069190eefb362b372d
Robert Gogete <gogeterobert@yahoo.com>
2022-05-09 19:33:36 +0300

    Update default values to match the constant type
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>

--- src/Fermyon.Nomad/Model/SidecarTask.cs
+++ src/Fermyon.Nomad/Model/SidecarTask.cs
@@ -46,7 +46,7 @@ namespace Fermyon.Nomad.Model
         /// <param name="resources">resources.</param>
         /// <param name="shutdownDelay">shutdownDelay.</param>
         /// <param name="user">user.</param>
-        public SidecarTask(Dictionary<string, Object> config = default(Dictionary<string, Object>), string driver = default(string), Dictionary<string, string> env = default(Dictionary<string, string>), string killSignal = default(string), long killTimeout = default(long), LogConfig logConfig = default(LogConfig), Dictionary<string, string> meta = default(Dictionary<string, string>), string name = default(string), Resources resources = default(Resources), long? shutdownDelay = default(long), string user = default(string))
+        public SidecarTask(Dictionary<string, Object> config = default(Dictionary<string, Object>), string driver = default(string), Dictionary<string, string> env = default(Dictionary<string, string>), string killSignal = default(string), long killTimeout = default(long), LogConfig logConfig = default(LogConfig), Dictionary<string, string> meta = default(Dictionary<string, string>), string name = default(string), Resources resources = default(Resources), long? shutdownDelay = default(long?), string user = default(string))
         {
             this.Config = config;
             this.Driver = driver;
